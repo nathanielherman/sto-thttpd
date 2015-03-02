29be94cfcea321deaa8c8714fa8bead3a496b113
Nathaniel Herman <nherman@college.harvard.edu>
2015-03-02 00:45:33 -0500

    Transactify mmc (mmap hashtable)

--- mmc.c
+++ mmc.c
@@ -45,6 +45,9 @@
 #include "mmc.h"
 #include "libhttpd.h"
 
+#include "sto/tm.h"
+#include "sto/Hashtable.hh"
+
 #ifndef HAVE_INT64T
 typedef long long int64_t;
 #endif
@@ -89,6 +92,22 @@ typedef struct MapStruct {
     struct MapStruct* next;
     } Map;
 
+struct MapKey {
+  ino_t ino;
+  dev_t dev;
+  off_t size;
+  time_t ct;
+  MapKey(ino_t ino, dev_t dev, off_t size, time_t ct) : ino(ino), dev(dev), size(size), ct(ct) {}
+  bool operator==(const MapKey& m2) const {
+    return ino == m2.ino && dev == m2.dev && size == m2.size && ct == m2.ct;
+  }
+};
+
+typedef struct {
+  void *addr;
+  int refcount;
+  time_t reftime;
+} MapVal;
 
 /* Globals. */
 static Map* maps = (Map*) 0;
@@ -100,15 +119,20 @@ static unsigned int hash_mask;
 static time_t expire_age = DEFAULT_EXPIRE_AGE;
 static off_t mapped_bytes = 0;
 
-
-
 /* Forwards. */
 static void panic( void );
-static void really_unmap( Map** mm );
-static int check_hash_size( void );
-static int add_hash( Map* m );
-static Map* find_hash( ino_t ino, dev_t dev, off_t size, time_t ct );
+static void really_unmap( const MapKey& key, const MapVal& val );
 static unsigned int hash( ino_t ino, dev_t dev, off_t size, time_t ct );
+
+
+
+class Hasher {
+ public:
+  size_t operator()(const MapKey& m) {
+    return hash( m.ino, m.dev, m.size, m.ct );
+  }
+};
+static Hashtable<MapKey, MapVal, INITIAL_HASH_SIZE, Hasher> hashtable;
 
 
 void*
@@ -137,165 +161,147 @@ mmc_map( char* filename, struct stat* sbP, struct timeval* nowP )
     else
         now = time( (time_t*) 0 );
 
-    /* See if we have it mapped already, via the hash table. */
-    if ( check_hash_size() < 0 )
-        {
-        syslog( LOG_ERR, "check_hash_size() failure" );
-        return (void*) 0;
-        }
-    m = find_hash( sb.st_ino, sb.st_dev, sb.st_size, sb.st_ctime );
-    if ( m != (Map*) 0 )
-        {
-        /* Yep.  Just return the existing map */
-        ++m->refcount;
-        m->reftime = now;
-        return m->addr;
-        }
+    void *addr_ret = NULL;
 
-    /* Open the file. */
-    fd = open( filename, O_RDONLY );
-    if ( fd < 0 )
-        {
-        syslog( LOG_ERR, "open - %m" );
-        return (void*) 0;
-        }
+    TM_BEGIN();
+    auto key = MapKey(sb.st_ino, sb.st_dev, sb.st_size, sb.st_ctime);
+    MapVal val;
+    // we try inserting first for two (annoying) reasons:
+    // *this way if the insert succeeds the rest of the transaction
+    //  is guaranteed to also succeed (whereas doing a get first
+    //  we can false conflict abort)
+    // *we don't have a proper value to insert yet, but if insert
+    //  succeeds then we'll have to mmap to get the proper value,
+    //  so if we just do the insert now we again can ensure
+    //  no aborts and do the mmap() without of leaking it from an abort
+    if (!hashtable.transInsert(TM_ARG key, val)) {
+      // ugh
+      hashtable.transGet(TM_ARG key, val);
+      // already have the mmap so increment refcount and we're done
+      val.refcount++;
+      val.reftime = now;
+      hashtable.transUpdate(TM_ARG key, val);
+      // TODO: this is kind of a funky workflow that wouldn't fit
+      // nicely in an atomic{} block, for instance.
+      // what sort of syntax could support this nicely??
+      TM_ARG_ALONE.commit();
+      return val.addr;
+    }
 
-    /* Find a free Map entry or make a new one. */
-    if ( free_maps != (Map*) 0 )
-        {
-        m = free_maps;
-        free_maps = m->next;
-        --free_count;
-        }
-    else
-        {
-        m = (Map*) malloc( sizeof(Map) );
-        if ( m == (Map*) 0 )
-            {
-            (void) close( fd );
-            syslog( LOG_ERR, "out of memory allocating a Map" );
-            return (void*) 0;
-            }
-        ++alloc_count;
-        }
-
-    /* Fill in the Map entry. */
-    m->ino = sb.st_ino;
-    m->dev = sb.st_dev;
-    m->size = sb.st_size;
-    m->ct = sb.st_ctime;
-    m->refcount = 1;
-    m->reftime = now;
+    // didn't find it so need to actually mmap it
+    val.refcount = 1;
+    val.reftime = now;
 
     /* Avoid doing anything for zero-length files; some systems don't like
     ** to mmap them, other systems dislike mallocing zero bytes.
     */
-    if ( m->size == 0 )
-        m->addr = (void*) 1;        /* arbitrary non-NULL address */
-    else
-        {
-        size_t size_size = (size_t) m->size;        /* loses on files >2GB */
+    if (key.size == 0) {
+      val.addr = (void*) 1; /* arbitrary non-NULL address */
+      hashtable.transUpdate(TM_ARG key, val);
+      TM_ARG_ALONE.commit();
+      return val.addr;
+    }
+
+    size_t size_size = (size_t) key.size;        /* loses on files >2GB */
+    
+    /* Open the file. */
+    fd = open( filename, O_RDONLY );
+    if ( fd < 0 ) {
+      syslog( LOG_ERR, "open - %m" );
+      // TODO: similarly, no obvious way to do this nicely syntactically
+      // (handling error conditions in a transaction)
+      TM_ARG_ALONE.silent_abort();
+      return (void*) 0;
+    }
+
 #ifdef HAVE_MMAP
-        /* Map the file into memory. */
-        m->addr = mmap( 0, size_size, PROT_READ, MAP_PRIVATE, fd, 0 );
-        if ( m->addr == (void*) -1 && errno == ENOMEM )
-            {
-            /* Ooo, out of address space.  Free all unreferenced maps
-            ** and try again.
-            */
-            panic();
-            m->addr = mmap( 0, size_size, PROT_READ, MAP_PRIVATE, fd, 0 );
-            }
-        if ( m->addr == (void*) -1 )
-            {
-            syslog( LOG_ERR, "mmap - %m" );
-            (void) close( fd );
-            free( (void*) m );
-            --alloc_count;
-            return (void*) 0;
-            }
+    /* Map the file into memory. */
+    val.addr = mmap( 0, size_size, PROT_READ, MAP_PRIVATE, fd, 0 );
+    if ( val.addr == (void*) -1 && errno == ENOMEM ) {
+      /* Ooo, out of address space.  Free all unreferenced maps
+      ** and try again.
+      */
+      panic();
+      val.addr = mmap( 0, size_size, PROT_READ, MAP_PRIVATE, fd, 0 );
+    }
+    if ( val.addr == (void*) -1 ) {
+      syslog( LOG_ERR, "mmap - %m" );
+      (void) close( fd );
+      TM_ARG_ALONE.silent_abort();
+      return (void*) 0;
+    }
 #else /* HAVE_MMAP */
-        /* Read the file into memory. */
-        m->addr = (void*) malloc( size_size );
-        if ( m->addr == (void*) 0 )
-            {
-            /* Ooo, out of memory.  Free all unreferenced maps
-            ** and try again.
-            */
-            panic();
-            m->addr = (void*) malloc( size_size );
-            }
-        if ( m->addr == (void*) 0 )
-            {
-            syslog( LOG_ERR, "out of memory storing a file" );
-            (void) close( fd );
-            free( (void*) m );
-            --alloc_count;
-            return (void*) 0;
-            }
-        if ( httpd_read_fully( fd, m->addr, size_size ) != size_size )
-            {
-            syslog( LOG_ERR, "read - %m" );
-            (void) close( fd );
-            free( (void*) m );
-            --alloc_count;
-            return (void*) 0;
-            }
+    /* Read the file into memory. */
+    val.addr = (void*) malloc( size_size );
+    if ( val.addr == (void*) 0 ) {
+      /* Ooo, out of memory.  Free all unreferenced maps
+      ** and try again.
+      */
+      // TODO: we should really abort at this point, since panic() does transactions of its own...
+      panic();
+      val.addr = (void*) malloc( size_size );
+    }
+    if ( val.addr == (void*) 0 ) {
+      syslog( LOG_ERR, "out of memory storing a file" );
+      (void) close( fd );
+      TM_ARG_ALONE.silent_abort();
+      return (void*) 0;
+    }
+    if ( httpd_read_fully( fd, val.addr, size_size ) != size_size ) {
+      syslog( LOG_ERR, "read - %m" );
+      (void) close( fd );
+      TM_ARG_ALONE.silent_abort();
+      return (void*) 0;
+    }
 #endif /* HAVE_MMAP */
-        }
     (void) close( fd );
 
-    /* Put the Map into the hash table. */
-    if ( add_hash( m ) < 0 )
-        {
-        syslog( LOG_ERR, "add_hash() failure" );
-        free( (void*) m );
-        --alloc_count;
-        return (void*) 0;
-        }
+    // finally have our mmap, we can "update" the value in the hashtable
+    hashtable.transUpdate(TM_ARG key, val);
 
-    /* Put the Map on the active list. */
-    m->next = maps;
-    maps = m;
-    ++map_count;
+    // need our mmap value to escape past transaction end
+    addr_ret = val.addr;
+
+    TM_END();
 
     /* Update the total byte count. */
-    mapped_bytes += m->size;
+    __sync_add_and_fetch(&mapped_bytes, sb.st_size);
+
+    // update map count
+    __sync_add_and_fetch(&map_count, 1);
 
     /* And return the address. */
-    return m->addr;
+    return addr_ret;
     }
 
 
 void
 mmc_unmap( void* addr, struct stat* sbP, struct timeval* nowP )
     {
-    Map* m = (Map*) 0;
-
-    /* Find the Map entry for this address.  First try a hash. */
-    if ( sbP != (struct stat*) 0 )
-        {
-        m = find_hash( sbP->st_ino, sbP->st_dev, sbP->st_size, sbP->st_ctime );
-        if ( m != (Map*) 0 && m->addr != addr )
-            m = (Map*) 0;
-        }
-    /* If that didn't work, try a full search. */
-    if ( m == (Map*) 0 )
-        for ( m = maps; m != (Map*) 0; m = m->next )
-            if ( m->addr == addr )
-                break;
-    if ( m == (Map*) 0 )
-        syslog( LOG_ERR, "mmc_unmap failed to find entry!" );
-    else if ( m->refcount <= 0 )
-        syslog( LOG_ERR, "mmc_unmap found zero or negative refcount!" );
+    // we really don't want to iterate through the whole table so we
+    // force passing the stat struct
+    assert(sbP);
+    MapKey key(sbP->st_ino, sbP->st_dev, sbP->st_size, sbP->st_ctime);
+    MapVal val;
+    TM_BEGIN();
+    bool found = hashtable.transGet(TM_ARG key, val);
+    if (!found) {
+      syslog( LOG_ERR, "mmc_unmap failed to find entry!" );
+      TM_ARG_ALONE.silent_abort();
+      return;
+    }
+    if (val.refcount <= 0) {
+      syslog( LOG_ERR, "mmc_unmap found zero or negative refcount!" );
+      TM_ARG_ALONE.silent_abort();
+      return;
+    }
+    val.refcount--;
+    if ( nowP != (struct timeval*) 0 )
+      val.reftime = nowP->tv_sec;
     else
-        {
-        --m->refcount;
-        if ( nowP != (struct timeval*) 0 )
-            m->reftime = nowP->tv_sec;
-        else
-            m->reftime = time( (time_t*) 0 );
-        }
+      val.reftime = time( (time_t*) 0 );
+    hashtable.transUpdate(TM_ARG key, val);
+    TM_END();
     }
 
 
@@ -303,8 +309,6 @@ void
 mmc_cleanup( struct timeval* nowP )
     {
     time_t now;
-    Map** mm;
-    Map* m;
 
     /* Get the current time, if necessary. */
     if ( nowP != (struct timeval*) 0 )
@@ -313,14 +317,31 @@ mmc_cleanup( struct timeval* nowP )
         now = time( (time_t*) 0 );
 
     /* Really unmap any unreferenced entries older than the age limit. */
-    for ( mm = &maps; *mm != (Map*) 0; )
-        {
-        m = *mm;
-        if ( m->refcount == 0 && now - m->reftime >= expire_age )
-            really_unmap( mm );
-        else
-            mm = &(*mm)->next;
+    auto end = hashtable.end();
+    for (auto it = hashtable.begin(); it != end; ++it) {
+      auto pair = *it;
+      MapKey key = pair.first;
+      MapVal val = pair.second;
+      // approximate
+      if (val.refcount == 0 && now - val.reftime >= expire_age) {
+        bool removed;
+        TM_BEGIN();
+        removed = false;
+        if (hashtable.transGet(TM_ARG key, val)) {
+          // make sure these conditions still hold
+          if (val.refcount == 0 && now - val.reftime >= expire_age) {
+            hashtable.transDelete(TM_ARG key);
+            removed = true;
+          }
         }
+        TM_END();
+        if (removed) {
+          really_unmap(key, val);
+        }
+        // TODO: after commit this node might be deallocated, so
+        // it's not really safe to just keep iterating from there...
+      }
+    }
 
     /* Adjust the age limit if there are too many bytes mapped, or
     ** too many or too few files mapped.
@@ -332,191 +353,85 @@ mmc_cleanup( struct timeval* nowP )
     else if ( map_count < DESIRED_MAX_MAPPED_FILES / 2 )
         expire_age = MIN( ( expire_age * 5 ) / 4, DEFAULT_EXPIRE_AGE * 3 );
 
-    /* Really free excess blocks on the free list. */
-    while ( free_count > DESIRED_FREE_COUNT )
-        {
-        m = free_maps;
-        free_maps = m->next;
-        --free_count;
-        free( (void*) m );
-        --alloc_count;
-        }
     }
-
 
 static void
 panic( void )
     {
-    Map** mm;
-    Map* m;
 
     syslog( LOG_ERR, "mmc panic - freeing all unreferenced maps" );
 
     /* Really unmap all unreferenced entries. */
-    for ( mm = &maps; *mm != (Map*) 0; )
-        {
-        m = *mm;
-        if ( m->refcount == 0 )
-            really_unmap( mm );
-        else
-            mm = &(*mm)->next;
+    auto end = hashtable.end();
+    for (auto it = hashtable.begin(); it != end; ++it) {
+      auto pair = *it;
+      MapKey key = pair.first;
+      MapVal val = pair.second;
+      // approximate
+      if (val.refcount == 0) {
+        bool removed;
+        TM_BEGIN();
+        removed = false;
+        if (hashtable.transGet(TM_ARG key, val)) {
+          // make sure these conditions still hold
+          if (val.refcount == 0) {
+            hashtable.transDelete(TM_ARG key);
+            removed = true;
+          }
         }
+        TM_END();
+        if (removed) {
+          really_unmap(key, val);
+        }
+        // TODO: after commit this node might be deallocated, so
+        // it's not really safe to just keep iterating from there...
+      }
+    }
+
     }
 
 
-static void
-really_unmap( Map** mm )
-    {
-    Map* m;
 
-    m = *mm;
-    if ( m->size != 0 )
-        {
+static void
+really_unmap( const MapKey& key, const MapVal& val )
+    {
+
+    if (!key.size)
+      return;
+
 #ifdef HAVE_MMAP
-        if ( munmap( m->addr, m->size ) < 0 )
-            syslog( LOG_ERR, "munmap - %m" );
-#else /* HAVE_MMAP */
-        free( (void*) m->addr );
-#endif /* HAVE_MMAP */
-        }
-    /* Update the total byte count. */
-    mapped_bytes -= m->size;
-    /* And move the Map to the free list. */
-    *mm = m->next;
-    --map_count;
-    m->next = free_maps;
-    free_maps = m;
-    ++free_count;
-    /* This will sometimes break hash chains, but that's harmless; the
-    ** unmapping code that searches the hash table knows to keep searching.
-    */
-    hash_table[m->hash_idx] = (Map*) 0;
+    if (munmap(val.addr, key.size) < 0)
+      syslog(LOG_ERR, "munmap - %m");
+#else
+    free(val.addr);
+#endif
+    
+    __sync_add_and_fetch(&mapped_bytes, key.size);
+
     }
 
 
 void
 mmc_term( void )
     {
-    Map* m;
-
-    while ( maps != (Map*) 0 )
-        really_unmap( &maps );
-    while ( free_maps != (Map*) 0 )
-        {
-        m = free_maps;
-        free_maps = m->next;
-        --free_count;
-        free( (void*) m );
-        --alloc_count;
-        }
+      // no way to even destruct a global Hashtable
     }
-
-
-/* Make sure the hash table is big enough. */
-static int
-check_hash_size( void )
-    {
-    int i;
-    Map* m;
-
-    /* Are we just starting out? */
-    if ( hash_table == (Map**) 0 )
-        {
-        hash_size = INITIAL_HASH_SIZE;
-        hash_mask = hash_size - 1;
-        }
-    /* Is it at least three times bigger than the number of entries? */
-    else if ( hash_size >= map_count * 3 )
-        return 0;
-    else
-        {
-        /* No, got to expand. */
-        free( (void*) hash_table );
-        /* Double the hash size until it's big enough. */
-        do
-            {
-            hash_size = hash_size << 1;
-            }
-        while ( hash_size < map_count * 6 );
-        hash_mask = hash_size - 1;
-        }
-    /* Make the new table. */
-    hash_table = (Map**) malloc( hash_size * sizeof(Map*) );
-    if ( hash_table == (Map**) 0 )
-        return -1;
-    /* Clear it. */
-    for ( i = 0; i < hash_size; ++i )
-        hash_table[i] = (Map*) 0;
-    /* And rehash all entries. */
-    for ( m = maps; m != (Map*) 0; m = m->next )
-        if ( add_hash( m ) < 0 )
-            return -1;
-    return 0;
-    }
-
-
-static int
-add_hash( Map* m )
-    {
-    unsigned int h, he, i;
-
-    h = hash( m->ino, m->dev, m->size, m->ct );
-    he = ( h + hash_size - 1 ) & hash_mask;
-    for ( i = h; ; i = ( i + 1 ) & hash_mask )
-        {
-        if ( hash_table[i] == (Map*) 0 )
-            {
-            hash_table[i] = m;
-            m->hash = h;
-            m->hash_idx = i;
-            return 0;
-            }
-        if ( i == he )
-            break;
-        }
-    return -1;
-    }
-
-
-static Map*
-find_hash( ino_t ino, dev_t dev, off_t size, time_t ct )
-    {
-    unsigned int h, he, i;
-    Map* m;
-
-    h = hash( ino, dev, size, ct );
-    he = ( h + hash_size - 1 ) & hash_mask;
-    for ( i = h; ; i = ( i + 1 ) & hash_mask )
-        {
-        m = hash_table[i];
-        if ( m == (Map*) 0 )
-            break;
-        if ( m->hash == h && m->ino == ino && m->dev == dev &&
-             m->size == size && m->ct == ct )
-            return m;
-        if ( i == he )
-            break;
-        }
-    return (Map*) 0;
-    }
-
 
 static unsigned int
 hash( ino_t ino, dev_t dev, off_t size, time_t ct )
-    {
-    unsigned int h = 177573;
+{
+  unsigned int h = 177573;
 
-    h ^= ino;
-    h += h << 5;
-    h ^= dev;
-    h += h << 5;
-    h ^= size;
-    h += h << 5;
-    h ^= ct;
+  h ^= ino;
+  h += h << 5;
+  h ^= dev;
+  h += h << 5;
+  h ^= size;
+  h += h << 5;
+  h ^= ct;
 
-    return h & hash_mask;
-    }
-
+  return h & hash_mask;
+}
 
 /* Generate debugging statistics syslog message. */
 void
@@ -524,8 +439,8 @@ mmc_logstats( long secs )
     {
     syslog(
         LOG_NOTICE, "  map cache - %d allocated, %d active (%lld bytes), %d free; hash size: %d; expire age: %ld",
-        alloc_count, map_count, (long long) mapped_bytes, free_count, hash_size,
+        map_count/*alloc_count==map_count*/, map_count, (long long) mapped_bytes, free_count, hash_size,
         expire_age );
-    if ( map_count + free_count != alloc_count )
+    if ( map_count + free_count != map_count )
         syslog( LOG_ERR, "map counts don't add up!" );
     }
